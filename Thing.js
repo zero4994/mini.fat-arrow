6de98c4ce59cf20799275b2a09f8e1e699f339b2
Code Chrysalis <codechrysalis@Codes-Mac-mini.local>
2019-01-07 15:02:46 +0900

    Completed basic requirements

--- Thing.js
+++ Thing.js
@@ -1,18 +1,22 @@
-var getEmpty = function() { return {} };
+var getEmpty = function() {
+  return {};
+};
 
 // multi-line
 var fall = function(time) {
-    var pos = 0;
-    var velocity = 0;
-    var g = 9.8;
-    for(var i = 0; i < time; i++) {
-        velocity += g;
-        pos += velocity;
-    }
-    return pos;
-}
+  var pos = 0;
+  var velocity = 0;
+  var g = 9.8;
+  for (var i = 0; i < time; i++) {
+    velocity += g;
+    pos += velocity;
+  }
+  return pos;
+};
 
 // no args
-var rand = function() { return Math.random() }
+var rand = function() {
+  return Math.random();
+};
 
-module.exports = { getEmpty, fall, rand }
+module.exports = { getEmpty, fall, rand };
--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-restricted-globals */
-const { expect } = require('chai');
-const Car = require('../Car');
-const Thing = require('../Thing');
+const { expect } = require("chai");
+const Car = require("../Car");
+const Thing = require("../Thing");
 
-describe('Car', () => {
-  it('should work', (done) => {
-    const car = new Car('Yan\'s pimpin ride');
+describe("Car", () => {
+  it("should work", (done) => {
+    const car = new Car("Yan's pimpin ride");
     car.drive();
     setTimeout(() => {
       car.stop();
@@ -17,20 +17,20 @@ describe('Car', () => {
   });
 });
 
-describe('Thing', () => {
-  it('should should return empty objects', () => {
+describe("Thing", () => {
+  it("should should return empty objects", () => {
     const expected = {};
     const actual = Thing.getEmpty();
     expect(actual).to.deep.equal(expected);
   });
 
-  it('should calculate fall position', () => {
+  it("should calculate fall position", () => {
     const expected = 539;
     const actual = Thing.fall(10);
     expect(actual).to.be.closeTo(expected, 0.1);
   });
 
-  it('should return random numbers', () => {
+  it("should return random numbers", () => {
     const expected = true;
     const actual = Thing.fall(10);
     expect(!isNaN(parseFloat(actual))).to.equal(expected);
